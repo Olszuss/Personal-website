8ab33a8608b1070c7215e14e31e50a853e5beb31
Adam Olszewski <adamolszewski96@gmail.com>
2023-06-20 21:33:36 +0200

    add starting files

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/", (req,res)=>{
+    res.render("home");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server started on port 3000");
+});
