93eedb24a806b30fcd7fa46babb24832989c5eb1
Adam Olszewski <adamolszewski96@gmail.com>
2023-06-25 15:58:20 +0200

    css @media update

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const ejs = require("ejs");
 
 const app = express();
 
-app.use(express.static("public"));
+
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 
 app.get("/", (req,res)=>{
     res.render("home");
+    console.log("Start")
 });
 
 
